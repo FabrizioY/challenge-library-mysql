aaeba79416bb638aeb14bfec6934ea15d39be5de
FabrizioY <89806040+FabrizioY@users.noreply.github.com>
2022-11-22 20:21:18 -0300

    Finalice los primeros puntos.

--- database/script.sql
+++ database/script.sql
@@ -1,1 +1,11 @@
 -- Create here your sql script
+create database biblioteca;
+use biblioteca;
+create table Autor (
+    id int,
+    nombre varchar(255)
+);
+ INSERT INTO Autor (nombre) VALUES ('Jorge Luis Borges')
+ INSERT INTO Autor (nombre) VALUES ('Stephen King')
+ INSERT INTO Autor (nombre) VALUES ('William Shakespeare')
+ALTER TABLE Autor MODIFY COLUMN id int auto_increment;
